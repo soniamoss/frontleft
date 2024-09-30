8ab49ebf5971cd9cf9ba0c6224eb10667b6075e6
Saud <saudchaudhary0@gmail.com>
2024-09-30 12:09:43 +0500

    main

--- app.config.ts
+++ app.config.ts
@@ -8,6 +8,12 @@ interface CustomConfig extends ExpoConfig {
       projectId: string;
     }
   };
+  "updates": {
+    "url": string;
+  };
+  "runtimeVersion": {
+    "policy": "appVersion";
+  };
 }
 
 export default ({ config }: { config: CustomConfig }) => ({
@@ -16,7 +22,13 @@ export default ({ config }: { config: CustomConfig }) => ({
     SUPABASE_URL: 'https://qkjjrcuoqnrjiimpmwpk.supabase.co',
     SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFrampyY3VvcW5yamlpbXBtd3BrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY1MTE2MjgsImV4cCI6MjAzMjA4NzYyOH0.L_9XB2JVCJ6NPPrMxeFqiDXCvH2DP939tIMSmxfxsto',
     eas: {
-        projectId: 'ca5854ab-4e3f-4d1e-a755-b695fe9a97ac',
+        projectId: 'bb85929a-7842-4ce0-ac4c-bbf0af877d0b',
     }
   },
+  "updates": {
+    "url": "https://u.expo.dev/bb85929a-7842-4ce0-ac4c-bbf0af877d0b"
+  },
+  "runtimeVersion": {
+    "policy": "appVersion"
+  }
 });
